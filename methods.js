e96127f5249a46b1acf86a3469084cb69bf30361
JosephCrispell <crispelljoseph@gmail.com>
2017-07-11 10:04:51 +0100

    InitialCommit

---
+++ methods.js
@@ -0,0 +1,26 @@
+/**
+ * Joseph Crispell
+ * General Functions to used on website
+ * Created 31/01/17
+ * To access methods add this line into html head section:
+ *      <head>
+ *          <script type="text/javascript" src="methods.js" ></script>
+ *      </head>
+ */
+
+/**
+ * Useful things:
+ *  - Uses same structure as Java
+ *  - If else
+ *  - Switch statement
+ *  - while loop
+ *  - for loop (in instead of :, foreach)
+ *  - break (can use labels -> labelName: -> break labelName;)
+ *  - continue
+ *
+ */
+
+// Simple test function to say hello
+function sayHelloToSomeone(name){
+    alert("Hello " + name + "!!");
+}
